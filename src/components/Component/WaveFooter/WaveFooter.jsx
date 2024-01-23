7daab518a111764e741087b66319d045ca6892e0
Gayan Dhanushka <gdhanushka95@gmail.com>
2024-01-24 04:01:16 +0530

    Dev(responsive issue)

---
+++ src/components/Component/WaveFooter/WaveFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WaveFooter = () => {
+  return (
+    <div class="container">
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+    </div>
+  );
+};
+
+export default WaveFooter;
